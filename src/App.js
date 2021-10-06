a81d8ee9101ccbe2b48b30ae53b9eff3c5f36f9a
Shivneel-Prasad <shivam201020@gmail.com>
2021-10-06 11:14:49 -0700

    Assingment Complete

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { BASE_URL, API_KEY } from './Constants/index'
 import "./App.css";
 
 function App() {
+  const [newData, setNewData] = useState([]);
+
+    useEffect(() => {
+      const getNewData = () => {
+        axios.get(`${BASE_URL}?api_key=${API_KEY}`)
+          .then(response => {
+            console.log(response);
+            setNewData(response.data)
+          }).catch(err => {
+            console.error(err);
+          })
+      };
+      getNewData();
+    }, [])
+  
+  // Display a loading message while the data is fetching
+  // props == newData & photoOfTheDay = url
+  if (!newData.url) return <h3>Loading...</h3>;
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      <div>
+        <h2>{newData.title}</h2>
+        <h3>{newData.date}</h3>
+        <p>{newData.explanation}</p>
+        <img src={newData.url} alt='Stars Everywhere'/>
+        <p>{newData.copyright}</p>
+      </div>
     </div>
   );
 }
